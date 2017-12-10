777a36bde2d516b5fe7834e5646736c543547d2f
BartekOgien <bartekogien2106@wp.pl>
2017-12-10 11:47:00 +0100

    Task 19.2

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -32,14 +32,14 @@ public class CoreConfiguration implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).
                 select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
-        
+
     }
 
     @Override
